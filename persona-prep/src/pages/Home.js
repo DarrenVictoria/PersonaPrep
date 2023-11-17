000da4e5f25d5c7d9b72834c3a06fe52b8a55072
DarrenVictoria <darrenvictoria2@gmail.com>
2023-11-17 12:57:48 +0530

    Again added
    Again added

--- persona-prep/src/pages/Home.js
+++ persona-prep/src/pages/Home.js
@@ -13,7 +13,7 @@ import DiversityPicture from '../assets/images/DiversityPicture.svg';
 import NForceLogo from '../assets/images/NForceLogo.svg';
 import {MDBCarousel,MDBCarouselInner,MDBCarouselItem,MDBContainer,MDBRow,MDBCol,MDBIcon,} from "mdb-react-ui-kit";
 
-//hello ci/cd
+//hello ci/cd again
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
