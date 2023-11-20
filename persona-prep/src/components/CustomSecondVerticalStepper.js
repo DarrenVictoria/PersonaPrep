d27bbd564f7abb08e3adcec402e6d2082665c326
IsuruUshanBandara <119607844+IsuruUshanBandara@users.noreply.github.com>
2023-11-20 16:13:35 +0530

    edited vertical stepper
    made the second stepper

---
+++ persona-prep/src/components/CustomSecondVerticalStepper.js
@@ -0,0 +1,80 @@
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Typography from '@mui/material/Typography';
+
+const StepperCustomIcon = ({ active, completed }) => {
+  return (
+    <div style={{ backgroundColor: 'gray', margin: '3px', borderRadius: '50%', width: '20px', height: '20px', padding: '5px', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+      <div style={{
+        backgroundColor: active || completed ? 'white' : 'black',
+        color: 'white',
+        height: '12px',
+        width: '12px',
+        borderRadius: '50%',
+        fontSize: '1rem',
+      }}>
+      </div>
+    </div>
+  );
+};
+
+const steps = [
+  {
+    label: 'Step A',
+  },
+  {
+    label: 'Step B',
+  },
+  {
+    label: 'Step C',
+  },
+  // Add more steps as needed
+];
+
+const CustomSecondVerticalStepper = forwardRef((props, ref) => {
+  const [activeStep, setActiveStep] = useState(0);
+
+  useImperativeHandle(ref, () => ({
+    handleNext: () => {
+        
+        
+      if (activeStep < steps.length - 1) {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      }
+    },
+    handleBack: () => {
+        
+        
+      if (activeStep > 0) {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+      }
+    },
+    getActiveStep: () => activeStep,
+    }), [activeStep]);
+
+  return (
+    <Box sx={{ maxWidth: 400 }}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step.label} style={{ color: 'black' }}>
+            <StepLabel
+              optional={
+                index === (steps.length - 1) ? (
+                  <Typography variant="caption"></Typography>
+                ) : null
+              }
+              StepIconComponent={StepperCustomIcon}
+            >
+              {step.label}
+            </StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
+});
+
+export default CustomSecondVerticalStepper;
--- persona-prep/src/components/Template.js
+++ persona-prep/src/components/Template.js
@@ -1,15 +1,16 @@
 import { Grid, Paper, badgeClasses, css, useMediaQuery } from '@mui/material';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Template.css';
 import CustomVerticalStepper from './CustomVerticalStepper.js';
+import CustomSecondVerticalStepper from './CustomSecondVerticalStepper.js';
 import Button from "@mui/material/Button";
 import {back} from './BackButton.js';
 import {next} from './NextButton.js';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import FacultyDetails from './StepperPages/FacultyDetails.js';
 import PersonalInfo from './StepperPages/PersonalInfo.js';
-import ContactDetails_1 from './StepperPages/ContactDetails1.js'
-import ContactDetails_2 from './StepperPages/ContactDetails2.js'
+import ContactDetails_1 from './StepperPages/ContactDetails1.js';
+import ContactDetails_2 from './StepperPages/ContactDetails2.js';
 import Education_1 from './StepperPages/Education1.js';
 import Education_2 from './StepperPages/Education2.js';
 import UniversityEducation from './StepperPages/UniversityEducation.js';
@@ -22,14 +23,53 @@ import Projects_3 from './StepperPages/Projects3.js';
 import Summary_1 from './StepperPages/Summary1.js';
 import Summary_2 from './StepperPages/Summary2.js';
 import logo from '../assets/logo/Persona Prep Light.png';
+
 const Template = () => {
     const cvs_instance = useRef();
-    const [currentPageIndex, setCurrentPageIndex] = useState(0)
+    const secondStepperRef=useRef();
+    const [currentPageIndex, setCurrentPageIndex] = useState(0);
+
+    useEffect(()=>{
+        
+            console.log("secondStepperRef:",secondStepperRef);
+        
+    },[secondStepperRef]);
+    
+    const handleFirstStepperNext = () => {
+        if (cvs_instance.current) {
+          cvs_instance.current.handleNext();
+          if (currentPageIndex < cvs_instance.current.getPageLength()) {
+            setCurrentPageIndex((prev) => prev + 1);
+          }
+        }
+      };
+    
+      const handleSecondStepperNext = () => {
+        if (secondStepperRef.current) {
+          secondStepperRef.current.handleNext();
+          console.log('handleSecondStepperNext called');
+          setCurrentPageIndex((prev) => prev + 1);
+        }
+      };
+    
+      const handleBack = () => {
+        if (currentPageIndex <= 15 && currentPageIndex > 0) {
+          if (cvs_instance.current) {
+            cvs_instance.current.handleBack();
+          }
+          setCurrentPageIndex((prev) => prev - 1);
+        } else if (currentPageIndex > 15 && secondStepperRef.current) {
+          secondStepperRef.current.handleBack();
+          setCurrentPageIndex((prev) => prev - 1);
+        }
+      };
+    
+      
 
     return(
 <div className="formtemp-page">
             <div className="formtemp-header-container">
-                <div class="logo"><img src={logo} alt="logo" style={{width:'120px'}}/></div>
+                <div className="logo"><img src={logo} alt="logo" style={{width:'120px'}}/></div>
                 <div className="formtemp-variable">
                     {
                         (currentPageIndex == 0) && <p className="formtemp-variablename"></p>
@@ -41,107 +81,129 @@ const Template = () => {
                         (currentPageIndex == 2) && <p className="formtemp-variablename">Contacts Details 1/2</p>
                     }
                     {
-                        (currentPageIndex == 3) && <p className="formtemp-variablename">Contacts Details 2/2</p>
+                        (currentPageIndex === 3) && <p className="formtemp-variablename">Contacts Details 2/2</p>
                     }
                     {
-                        (currentPageIndex == 4) && <p className="formtemp-variablename">Summary Data 1/2</p>
+                        (currentPageIndex === 4) && <p className="formtemp-variablename">Summary Data 1/2</p>
                     }
                     {
-                        (currentPageIndex == 5) && <p className="formtemp-variablename">Summary Data 2/2</p>
+                        (currentPageIndex === 5) && <p className="formtemp-variablename">Summary Data 2/2</p>
                     }
                     {
-                        (currentPageIndex == 6) && <p className="formtemp-variablename">School Education 1/2</p>
+                        (currentPageIndex === 6) && <p className="formtemp-variablename">School Education 1/2</p>
                     }
                     {
-                        (currentPageIndex == 7) && <p className="formtemp-variablename">School Education 2/2</p>
+                        (currentPageIndex === 7) && <p className="formtemp-variablename">School Education 2/2</p>
                     }
                     {
-                        (currentPageIndex == 8) && <p className="formtemp-variablename">University Education</p>
+                        (currentPageIndex === 8) && <p className="formtemp-variablename">University Education</p>
                     }
                     {
-                        (currentPageIndex == 9) && <p className="formtemp-variablename">Work Experience 1/3</p>
+                        (currentPageIndex === 9) && <p className="formtemp-variablename">Work Experience 1/3</p>
                     }
                     {
-                        (currentPageIndex == 10) && <p className="formtemp-variablename">Work Experience 2/3</p>
+                        (currentPageIndex === 10) && <p className="formtemp-variablename">Work Experience 2/3</p>
                     }
                     {
-                        (currentPageIndex == 11) && <p className="formtemp-variablename">Work Experience 3/3</p>
+                        (currentPageIndex === 11) && <p className="formtemp-variablename">Work Experience 3/3</p>
                     }
                     {
-                        (currentPageIndex == 12) && <p className="formtemp-variablename">Project Experience 1/3</p>
+                        (currentPageIndex === 12) && <p className="formtemp-variablename">Project Experience 1/3</p>
                     }
                     {
-                        (currentPageIndex == 13) && <p className="formtemp-variablename">Project Experience 2/3</p>
+                        (currentPageIndex === 13) && <p className="formtemp-variablename">Project Experience 2/3</p>
                     }
                     {
-                        (currentPageIndex == 14) && <p className="formtemp-variablename">Project Experience 3/3</p>
+                        (currentPageIndex === 14) && <p className="formtemp-variablename">Project Experience 3/3</p>
                     }
+                    {
+                        (currentPageIndex === 15) && <p className="formtemp-variablename">Project Experience 3/3</p>
+                    }
+                    {
+                        (currentPageIndex === 16) && <p className="formtemp-variablename">summary1</p>
+                    }
+                    {
+                        (currentPageIndex === 17) && <p className="formtemp-variablename">summary2</p>
+                    }
+                    
                     
                 </div>
             </div>
             <div className="formtemp-bodyform">
                 <Grid container spacing={2}>
                     <Grid xs={4} style={{padding: "25px"}}>
-                        <CustomVerticalStepper ref={cvs_instance} ></CustomVerticalStepper>
+                        {currentPageIndex<=14?(
+                        <CustomVerticalStepper ref={cvs_instance} ></CustomVerticalStepper>) : (<CustomSecondVerticalStepper ref={secondStepperRef}></CustomSecondVerticalStepper>)}
                     </Grid>
                     <Grid xs={8} style={{backgroundColor: "#D9D9D9", borderRadius: "0px 0px 50px 0px", paddingLeft: "10px", borderLeft: "2px solid black"}}>
                     <form style={{height: '100%', position: 'relative'}}>
                         <div style={{marginBottom: '75px'}}>
                             {
-                                (currentPageIndex == 0) && <FacultyDetails></FacultyDetails>
+                                (currentPageIndex === 0) && <FacultyDetails></FacultyDetails>
                             }
                             {
-                                (currentPageIndex == 1) && <PersonalInfo></PersonalInfo>
+                                (currentPageIndex === 1) && <PersonalInfo></PersonalInfo>
                             }
                             {
-                                (currentPageIndex == 2) && <ContactDetails_1></ContactDetails_1>
+                                (currentPageIndex === 2) && <ContactDetails_1></ContactDetails_1>
                             }
                             {
-                                (currentPageIndex == 3) && <ContactDetails_2></ContactDetails_2>
+                                (currentPageIndex === 3) && <ContactDetails_2></ContactDetails_2>
                             }
                             {
-                                (currentPageIndex == 4) && <Summary_1></Summary_1>
+                                (currentPageIndex === 4) && <Summary_1></Summary_1>
                             }
                             {
-                                (currentPageIndex == 5) && <Summary_2></Summary_2>
+                                (currentPageIndex === 5) && <Summary_2></Summary_2>
                             }
                             {
-                                (currentPageIndex == 6) && <Education_1></Education_1>
+                                (currentPageIndex === 6) && <Education_1></Education_1>
                             }
                             {
-                                (currentPageIndex == 7) && <Education_2></Education_2>
+                                (currentPageIndex === 7) && <Education_2></Education_2>
                             }
                             {
-                                (currentPageIndex == 8) && <UniversityEducation></UniversityEducation>
+                                (currentPageIndex === 8) && <UniversityEducation></UniversityEducation>
                             }
                             {
-                                (currentPageIndex == 9) && <WorkExperience_1></WorkExperience_1>
+                                (currentPageIndex === 9) && <WorkExperience_1></WorkExperience_1>
                             }
                             {
-                                (currentPageIndex == 10) && <WorkExperience_2></WorkExperience_2>
+                                (currentPageIndex === 10) && <WorkExperience_2></WorkExperience_2>
                             }
                             {
-                                (currentPageIndex == 11) && <WorkExperience_3></WorkExperience_3>
+                                (currentPageIndex === 11) && <WorkExperience_3></WorkExperience_3>
                             }
                             {
-                                (currentPageIndex == 12) && <Projects_1></Projects_1>
+                                (currentPageIndex === 12) && <Projects_1></Projects_1>
                             }
                             {
-                                (currentPageIndex == 13) && <Projects_2></Projects_2>
+                                (currentPageIndex === 13) && <Projects_2></Projects_2>
                             }
                             {
-                                (currentPageIndex == 14) && <Projects_3></Projects_3>
+                                (currentPageIndex === 14) && <Projects_3></Projects_3>
                             }
+                            {
+                                (currentPageIndex === 15) && <Projects_3></Projects_3>
+                            }
+                            {
+                                (currentPageIndex === 16) && <Summary_2></Summary_2>
+                            }
+                            {
+                                (currentPageIndex === 17) && <Summary_1></Summary_1>
+                            }
+                            
+                            
                             
                         </div>
 
                         <Grid container spacing={2} style={{position: 'absolute', bottom: 10}}>
                             <Grid xs={6}>
-                                <Button startIcon={<ArrowBackIcon />} style={back} onClick={() => {cvs_instance.current.handleBack(); if (currentPageIndex > 0){setCurrentPageIndex(currentPageIndex - 1)}}}>Go Back</Button>
+                                <Button startIcon={<ArrowBackIcon />} style={back} onClick={() => { if (currentPageIndex <= 14 && cvs_instance.current) { cvs_instance.current.handleBack(); if (currentPageIndex > 0) { setCurrentPageIndex(currentPageIndex - 1); } } else if (secondStepperRef.current) {  secondStepperRef.current.handleBack(); setCurrentPageIndex((prevIndex) => prevIndex - 1); }}}>Go Back</Button>
                             </Grid>
                                 
                             <Grid xs={6}>
-                                <Button style={next} onClick={() => {cvs_instance.current.handleNext(); if (currentPageIndex < cvs_instance.current.getPageLength()){setCurrentPageIndex(currentPageIndex + 1)}}}>Next Step</Button>
+                                <Button style={next} onClick={() => {if(currentPageIndex<=14 && cvs_instance.current){cvs_instance.current.handleNext(); if (currentPageIndex < cvs_instance.current.getPageLength()){setCurrentPageIndex((prev)=>prev+1);}}else if(secondStepperRef.current){secondStepperRef.current.handleNext();setCurrentPageIndex((prev)=>prev+1);}}}>Next Step</Button>
                             </Grid>
                         </Grid>
                     </form>
@@ -152,7 +214,7 @@ const Template = () => {
             
             
         </div>
-    )
+    );
 
-}
-export default Template
+};
+export default Template;
