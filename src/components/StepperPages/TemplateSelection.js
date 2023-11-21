7b17ae300e67de695cff2de326ae536137ecc6ca
IsuruUshanBandara <119607844+IsuruUshanBandara@users.noreply.github.com>
2023-11-21 14:10:34 +0530

    Added some changes to the Templateselection section
    added more thing to the template selection section

--- src/components/StepperPages/TemplateSelection.js
+++ src/components/StepperPages/TemplateSelection.js
@@ -1,17 +1,85 @@
 import React from 'react';
-import '../TemplateSelection.css'; 
-const TemplateSelection = () => {
-    return(
-        <div>
-            <div className='TemplateSelection-Container'>
-                <div className='TemplateSelection-LeftColumn'>Left column</div>
-                <div className='TemplateSelection-RightColumn'>
-                    <div className='TemplateSelection-RightRow'>Right Row 1</div>
-                    <div className='TemplateSelection-RightRow'>Right Row 2</div>
-                </div>
-            </div>
-        </div>
-    )
+import '../TemplateSelection.css';
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from "@mui/material/Box";
 
-}
-export default TemplateSelection
+const buttons = [<Button key="one">One</Button>];
+const buttons2 = [<Button key="two">Two</Button>];
+
+const TemplateSelection = () => {
+  return (
+    <div>
+      <div className='TemplateSelection-Container'>
+        <div className='TemplateSelection-LeftColumn'>Templates</div>
+        <div className='TemplateSelection-RightColumn'>
+          <div className='TemplateSelection-RightRow'>
+             <p>Color Themes Section</p>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                "& > *": {
+                  m: 0.5
+                }
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  "& > *": {
+                    m: 0.5
+                  }
+                }}
+              >
+                <ButtonGroup
+                  orientation="vertical"
+                  aria-label="vertical contained button group"
+                  variant="contained"
+                >
+                  {buttons}
+                </ButtonGroup>
+                <ButtonGroup
+                  orientation="vertical"
+                  aria-label="vertical contained button group"
+                  variant="contained"
+                >
+                  {buttons}
+                </ButtonGroup>
+              </Box>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  "& > *": {
+                    m: 0.5
+                  }
+                }}
+              >
+                <ButtonGroup
+                  orientation="vertical"
+                  aria-label="vertical contained button group"
+                  variant="contained"
+                >
+                  {buttons2}
+                </ButtonGroup>
+                <ButtonGroup
+                  orientation="vertical"
+                  aria-label="vertical contained button group"
+                  variant="contained"
+                >
+                  {buttons2}
+                </ButtonGroup>
+              </Box>
+            </Box>
+            {/* End Color Themes Section */}
+          </div>
+          <div className='TemplateSelection-RightRow'>Typography</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TemplateSelection;
