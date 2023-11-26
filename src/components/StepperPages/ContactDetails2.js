d5987226b93cfab7f4aa567d2d617dd54a32068a
IsuruUshanBandara <119607844+IsuruUshanBandara@users.noreply.github.com>
2023-11-26 14:30:51 +0530

    updated the maintemplate and the contact2/2
    added the margin to the form main template have to do the alignment with the stepper. In contact  2/2  added the aligments and also added the icons and in the image folder the images starting form icon is related to the contact page. still there are some changes to do in it and will do

--- src/components/StepperPages/ContactDetails2.js
+++ src/components/StepperPages/ContactDetails2.js
@@ -11,6 +11,11 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';// for the left column
 import Checkbox from '@mui/material/Checkbox';// for the left column
 import Avatar from '@mui/material/Avatar';// for the left column
 import TextField from '@mui/material/TextField';// for the left column
+import github from '../../assets/images/icongithub.svg';
+import linkedIn from '../../assets/images/iconlinkedin.svg';
+import twitter from '../../assets/images/icontwitter.svg';
+import stackoverflow from '../../assets/images/iconstackoverflow.png';
+import medium from '../../assets/images/iconmedium.svg';
 const ContactDetails_2 = () => {
     // State to keep track of checked items
   const [checked, setChecked] = React.useState([1]);
@@ -40,56 +45,58 @@ const ContactDetails_2 = () => {
 
     return(
     <div className='Contactdetails2-Maindiv'>
-      {/* Left Column */}
-        <div className='Contactdetails2-Subdiv'>
-                <div className='Contactdetails2-LeftColumn'>
-            
-                    {/* CheckboxListWithTextField component code */}
-                    <List dense style={{ width: '100%', maxWidth: 700, bgcolor: 'white', height: '389px' }}>
-            {[0, 1, 2, 3].map((value, index) => {
-                const labelId = `checkbox-list-secondary-label-${value}`;
-                const sampleNames = ['John Doe', 'Jane Doe', 'Alice Smith', 'Bob Johnson'];
-                const sampleImages = [
-                'https://via.placeholder.com/150',
-                'https://via.placeholder.com/150',
-                'https://via.placeholder.com/150',
-                'https://via.placeholder.com/150',
-                ];
+     
+        
+      <div className='Contactdetails2-LeftColumn'>
+        
+                {/* CheckboxListWithTextField component code */}
+          <h2 style={{marginBottom:'5px'}}>Social network accounts</h2> <p style={{marginTop:'2px'}}>Indicate the desired communication method</p> 
+              
+        <List dense style={{ width: '100%', maxWidth: 700, bgcolor: 'white', height: '389px' }}>
+              {[0, 1, 2, 3, 4].map((value, index) => {
+                  const labelId = `checkbox-list-secondary-label-${value}`;
+                  const sampleNames = ['GitHub', 'Linken In', 'Twitter', 'Stackoverflow','Medium'];
+                  const sampleImages = [github,linkedIn,twitter,stackoverflow,medium];
 
-                return (
-                <ListItem key={value} disablePadding>
-                    <ListItemButton>
-                    <ListItemAvatar>
-                        <Avatar alt={`Avatar of ${sampleNames[value]}`} src={sampleImages[value]} />
-                    </ListItemAvatar>
-                    <ListItemText id={labelId} primary={sampleNames[value]} />
-                    </ListItemButton>
-                    <Checkbox
-                    edge="end"
-                    onChange={handleToggle(value)}
-                    checked={checked.indexOf(value) !== -1}
-                    inputProps={{ 'aria-labelledby': labelId }}
-                    />
-                    <TextField
-                    label="Text Field"
-                    variant="outlined"
-                    size="small"
-                    style={{ marginLeft: '10px' }}
-                    value={textValues[index]} // Controlled input value
-                    onChange={handleTextFieldChange(index)} // Handle text field changes
-                    />
-                </ListItem>
-                );
-            })}
-            </List>
-                </div>
+            return (
+            <ListItem key={value} disablePadding>
+                <ListItemButton>
+                <ListItemAvatar>
+                    <Avatar alt={`Avatar of ${sampleNames[value]}`} src={sampleImages[value]} sx={{width:'24px',height:'24px'}}/>
+                </ListItemAvatar>
+                <ListItemText id={labelId} primary={sampleNames[value]} />
+                </ListItemButton>
+                <Checkbox
+                edge="end"
+                onChange={handleToggle(value)}
+                checked={checked.indexOf(value) !== -1}
+                inputProps={{ 'aria-labelledby': labelId }}
+                />
+                <TextField
+                label="@profile"
+                variant="outlined"
+                size="small"
+                /*below inputprops and the inputlabelprop is to add styling for the text box and the place holder*/ 
+                InputProps={{
+                style:{ marginLeft: '10px',borderRadius:"20px",backgroundColor:"white",color:"black",}}}
+                InputLabelProps={{
+                  style:{color:"#CBCBCB"}
+                }}
+                value={textValues[index]} // Controlled input value
+                onChange={handleTextFieldChange(index)} // Handle text field changes
+                />
+            </ListItem>
+            );
+          })}
+        </List>
+      </div>
             
 
       
 
-      {/* Right Column */}
+     
       <div className='Contactdetails2-RightColumn'>
-      <Card variant="outlined" sx={{ height: '389px', width: '363px', borderRadius: '15px' }}>
+        <Card variant="outlined" sx={{ height:'100%',maxHeight: '389px', width:'100%',maxWidth: '363px',borderRadius:'15px'}}>
           <CardContent sx={{ textAlign: 'center' }}>
             <Typography variant="h5" component="div">
               Welcome back, Tom
@@ -98,11 +105,11 @@ const ContactDetails_2 = () => {
         </Card>
         
       </div>
-      </div>
+        
       
     </div>
     
-    )
+  )
 
 }
 export default ContactDetails_2
