7b762583bae6b473098818d5179c71d9af2b934e
IsuruUshanBandara <119607844+IsuruUshanBandara@users.noreply.github.com>
2023-12-02 15:32:21 +0530

    added the addportpflio feature.Have to make the styling
    done with the add and delete in the add portfolio feature

--- src/components/StepperPages/ContactDetails1.js
+++ src/components/StepperPages/ContactDetails1.js
@@ -14,14 +14,37 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';// for the right colum
 //import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add'
+import CloseIcon from '@mui/icons-material/Close';
 import Button from '@mui/material/Button';
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import cphone from '../../assets/images/iconcphone.svg';
 import cmail from '../../assets/images/iconcmail.svg';
 import cfolder from '../../assets/images/iconcfolder.svg';
+import { useState } from 'react';
+
+const predefinedButtonName = ['github','figma','behance','linkedin','facebook','whatsapp','instragram','twitter'];
 const ContactDetails_1 = () => {
-  
+  //below usestate is to keep the user entered url
+  const [inputUrl, setInputUrl] = useState(''); 
+  //to keep array of already shown buttons
+  const[shownbuttons,setShownButtons] = useState([]);
+  const checkUrl = () =>{
+    //converting the url to lowercase
+    const lowerCaseUrl = inputUrl.toLowerCase();
+
+    //checking the lowerCaseUrl with the predefined button for matches
+    const matchedresult = predefinedButtonName.filter(buttonName => lowerCaseUrl.includes(buttonName));
+
+    // updating the shownbutton 
+    setShownButtons(prevButtons =>{
+      const uniqueButtons = Array.from(new Set([...prevButtons,...matchedresult]));
+      return uniqueButtons
+    });
+  };
+  const removeButton = (buttonName) => {
+    setShownButtons(prevButtons => prevButtons.filter(button => button !== buttonName));
+  };
     return(
     <div className='Contactdetails1-Maindiv'>
      
@@ -71,21 +94,27 @@ const ContactDetails_1 = () => {
                   <TextField type="text" variant="outlined" fullWidth required InputProps={{ style: {borderRadius: '25px',backgroundColor: 'white',},}}/>
                 
               </Grid>
+              <Grid item xs={12} style={{ marginBottom: '-15px' }}>
+              <Typography>Other Portfolio links</Typography>
+              </Grid>
+              <Grid item xs={11}>
+                  <TextField type="text" variant="outlined" fullWidth InputProps={{ style: {borderRadius: '25px',backgroundColor: 'white',},}} onChange={(e)=>setInputUrl(e.target.value)}/>
+              </Grid>
+              <Grid item xs={1} style={{ display: 'flex', alignItems: 'center' }}>
+                  <IconButton color="primary"style={{ backgroundColor: 'black', borderRadius: '50%',width:'22px',height:'22px' }} onClick={checkUrl}><AddIcon style={{ color: 'white' }} /></IconButton>
+                
+              </Grid>
               <Grid item xs={12}>
                 
-                  <Typography>Other Portfolio links <IconButton color="primary"style={{ backgroundColor: 'black', borderRadius: '50%',width:'22px',height:'22px' }}><AddIcon style={{ color: 'white' }} /></IconButton></Typography>
-                  <TextField type="text" variant="outlined" fullWidth InputProps={{ style: {borderRadius: '25px',backgroundColor: 'white',},}}/>
-                
+              {shownbuttons.map((buttonName)=>(
+                <button key={buttonName} style={{backgroundColor: 'black', color:'white', borderRadius:'25px'}}>{buttonName}<IconButton color='primary' style={{backgroundColor: 'black', border: '1px solid white',borderRadius: '50%', marginLeft: '5px', width: '15px', height: '15px' }} onClick={()=>removeButton(buttonName)}><CloseIcon style={{color: 'white', fontSize: '12px'}}/></IconButton></button>
+              ))}
               </Grid>
               <Grid item xs={3}>
+                {predefinedButtonName.map((buttonName)=>(
+                  <button key={buttonName} style={{display:'none'}}>{buttonName}</button>
+                ))}
                 
-              <Button variant="outlined" fullWidth style={{ borderRadius: '25px', backgroundColor: 'black',color:'white',fontSize:'20px',fontFamily:'inter',fontStyle:'normal',fontWeight:700 ,textTransform: 'none'}}>Figma</Button>
-              
-              </Grid>
-              <Grid item xs={3}>
-                
-                
-              <Button variant="outlined" fullWidth style={{ borderRadius: '25px', backgroundColor: 'black',color:'white',fontSize:'20px',fontFamily:'inter',fontStyle:'normal',fontWeight:700 ,textTransform: 'none' }}>Behance</Button>
               
               </Grid>
             </Grid>
