41e7db676dc54a2ef4a3802b41510148a455d57b
IsuruUshanBandara <119607844+IsuruUshanBandara@users.noreply.github.com>
2024-01-02 19:38:03 +0530

    done with the clubs and societies
    done with the clubs and societies but have to do some adjustments to make it mobile responsive and a small change to do in order to changethe font weight or the color of the customize hook label

--- src/components/StepperPages/Clubs.js
+++ src/components/StepperPages/Clubs.js
@@ -1,37 +1,25 @@
 import React from 'react';
-import '../Certification1.css';
-import Card from '@mui/material/Card'; //this is for the card in the right column
-import CardContent from '@mui/material/CardContent'; //this is for the card in the right column
+import '../Clubs.css';
 import Typography from '@mui/material/Typography'; //this is for the card in the right column
-import Avatar from '@mui/material/Avatar';// for the right column
-import List from '@mui/material/List';// for the right column
-import ListItem from '@mui/material/ListItem';// for the right column
-import ListItemText from '@mui/material/ListItemText';// for the right column
-import ListItemAvatar from '@mui/material/ListItemAvatar';// for the right column
 import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
-import cdiary from '../../assets/images/iconcdiary.svg';
-import ccalander from '../../assets/images/iconccalander.svg';
-import chat from '../../assets/images/iconchat.svg';
 import EditableChoose from '../EditableSelectOption';
 import FormGroup from "@mui/material/FormGroup";//for the check box
 import FormControlLabel from "@mui/material/FormControlLabel";//for the check box
 import Checkbox from "@mui/material/Checkbox";//for the check box
 import CustomizedHook from '../TextfieldButtonDataDisplay';
-import { useState } from 'react';
 const Clubs = () => {
     const[month,setMonth]= React.useState("");
     const[year,setYear]= React.useState("");
     const dataset = [{data:"c#"},{data:"Java"}];
     return(
-        <div className='Certification1-Maindiv'>
-        <div className='Certification1-LeftColumn'>
+        <div className='Clubs-Maindiv'>
+        <div className='Clubs-LeftColumn'>
         {/*<Box sx={{ flexGrow: 1 }}>*/}
         <Grid container spacing={2} >
-          
+        <Grid item xs={12} style={{ marginBottom: '-40px' }}>
+        <Typography>*Club / Society you were a part of ?</Typography>
+          </Grid>
           <Grid item xs={12} >
-            
-              <Typography>*Issuing Organization</Typography>
               <EditableChoose
                 options={["Month","January", "February", "March", "April","May", "June", "July", "August","September", "October", "November", "December"]}
                 onSelect={setMonth}
@@ -42,7 +30,7 @@ const Clubs = () => {
             
           </Grid>
           <Grid item xs={12} style={{ marginBottom: '-40px' }}>
-          <Typography>*Issue Date</Typography>
+          <Typography>*Start Date</Typography>
           </Grid>
           <Grid item xs={6}>
             
@@ -66,7 +54,7 @@ const Clubs = () => {
             
           </Grid>
           <Grid item xs={12} style={{ marginBottom: '-40px' }}>
-          <Typography>*Expiration Date</Typography>
+          <Typography>*End Date</Typography>
           </Grid>
           <Grid item xs={6}>
             
@@ -102,7 +90,7 @@ const Clubs = () => {
           </Grid>
           <Grid item xs={11}>
             
-            <CustomizedHook width={400} height={120} data={dataset} label='Roles Played'/>
+            <CustomizedHook width={457} height={105} data={dataset} label='Roles Played'/>
           
             
           </Grid>
@@ -116,58 +104,12 @@ const Clubs = () => {
   
 
  
-  <div className='Certification1-RightColumn'>
-    <Card variant="outlined" sx={{ height:'100%',maxHeight: '450px', width:'100%',maxWidth: '363px',borderRadius:'15px',overflowY:'auto',overflowX:'auto','@media (min-width:768px)':{overflowY:'hidden',},}}className='Contactdetails2-RightColumnCard'>
-      <CardContent >
-        <Typography variant="h5" component="div"sx={{ textAlign: 'center' }}>
-            Certification Tips
-        </Typography>
-        <List>
-          <ListItem >
-          <ListItemAvatar>
-            <Avatar sx={{ /*width: '38.732px', height: '39.022px',*/ borderRadius: '12px'/*, display: 'flex', justifyContent: 'center', alignItems: 'center'*/ }}>
-            <img src={cdiary} alt="Custom Icon" style={{ width: '27px', height: '31px' }}/>
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText>
-            <Typography variant='body1' >
-            Fill in all the information about your most recent Certification to fill in this section.
-            </Typography>
-          </ListItemText>
-          </ListItem>
-          <ListItem >
-          <ListItemAvatar>
-            <Avatar sx={{ /*width: '38.732px', height: '39.022px',*/ borderRadius: '12px' ,display: 'flex', justifyContent: 'center', alignItems: 'center'  }}>
-          <img src={ccalander} alt="Custom Icon" style={{ width: 'var(--40,40px)', height: '35.666px' }} />
-          </Avatar>
-          </ListItemAvatar>
-          <ListItemText>
-          <Typography variant='body1'>
-          Ensure your certifications are up-to-date by double-checking their expiration dates.
-          </Typography>
-          </ListItemText>
-          </ListItem>
-          <ListItem >
-          <ListItemAvatar>
-            {/*aligning the image did not work have to look into it*/ }
-            <Avatar sx={{/* width: '38.732px', height: '39.022px',*/ borderRadius: '12px',display: 'flex', justifyContent: 'center', alignItems: 'center'  }}>
-          <img src={chat} alt="Custom Icon" style={{ width: '41px', height: '39px' ,}} />
-          </Avatar>
-          </ListItemAvatar>
-          <ListItemText>
-          <Typography variant='body1'>
-          Arrange your certifications logically according to the most relevant ones for the position you're applying for.
-          </Typography>
-          </ListItemText>
-          </ListItem>
-        </List>
-      </CardContent>
-    </Card>
-    
-        </div>
-</div>
+    <div className='Clubs-RightColumn'>
+      <CustomizedHook width={457} height={373} data={dataset} label='Roles Played'/>
+    </div>
+</div>  
 
     )
 
 }
-export default Clubs
+export default Clubs;
