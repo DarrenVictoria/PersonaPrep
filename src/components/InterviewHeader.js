ef308360581e7c4bc983a2c6fb4955643c31d0fb
Ishuwaraa <115783906+Ishuwaraa@users.noreply.github.com>
2024-01-24 08:04:48 +0530

    Added the interviewDisplay page
    Added the interviewDisplay page

---
+++ src/components/InterviewHeader.js
@@ -0,0 +1,22 @@
+import InterviewTitle from './InterviewTitle'
+import InterviewDate from './InterviewDate'
+import BlogShareIcons from './BlogShareIcons';
+import InterviewTag from './InterviewTag';
+
+const InterviewHeader = ({position, date, tag, tag1, tag2}) => {
+    return ( 
+        <div className="interviewDisplay-header">
+            <div className='interviewDisplay-leftCol'>
+                <InterviewTitle position={position}/>
+                <InterviewTag tag={tag} />
+            </div>
+
+            <div className='interviewDisplay-rightCol'>
+                <InterviewDate date={date} />
+                <BlogShareIcons title={position} tag1={tag1} tag2={tag2}/>
+            </div>
+        </div>
+     );
+}
+
+export default InterviewHeader;
--- src/components/TranscriptCard.js
+++ src/components/TranscriptCard.js
@@ -4,14 +4,20 @@ import InterviewDetails from './InterviewDetails';
 import Thumbnail from './Thumbnail';
 
 export default function TranscriptCard({name, date, position, detail, path, category}){
+    const handleClick = () => {
+        window.location.href = '/interviewDisplay';
+    }
+
     return(
         // <div className={`interviewBank-transcriptCard ${category !== '' && 'interviewBank-hideCard'}`} data-category={category}>
         <div className='interviewBank-transcriptCard'>
-            <div className='interviewBank-transcriptCardDetails'>
-                <TranscriptLabels name={name} date={date} />
-                <JobPosition position={position} />
-                <InterviewDetails detail={detail} />
-            </div>                 
+            <a href="#" style={{textDecoration: 'none', color: 'black'}} onClick={handleClick}>
+                <div className='interviewBank-transcriptCardDetails'>
+                    <TranscriptLabels name={name} date={date} />
+                    <JobPosition position={position} />
+                    <InterviewDetails detail={detail} />
+                </div>                 
+            </a>              
             <Thumbnail path={path} />
         </div>
     )
