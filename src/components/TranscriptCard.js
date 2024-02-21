4f4313e24ff21981ed63e9a00d0e5fe91146d3f6
Ishuwaraa <115783906+Ishuwaraa@users.noreply.github.com>
2024-02-21 23:15:53 +0530

    Connected interview bank pages to the db
    Connected interview bank pages to the db

--- src/components/TranscriptCard.js
+++ src/components/TranscriptCard.js
@@ -3,14 +3,14 @@ import JobPosition from './JobPosition';
 import InterviewDetails from './InterviewDetails';
 import Thumbnail from './Thumbnail';
 
-export default function TranscriptCard({name, date, position, detail, path, category}){
+export default function TranscriptCard({name, date, position, detail, path, category, id}){
     const handleClick = () => {
-        window.location.href = '/interviewDisplay';
+        window.location.href = `/interviewDisplay?id=${id}`;
     }
 
     return(
         // <div className={`interviewBank-transcriptCard ${category !== '' && 'interviewBank-hideCard'}`} data-category={category}>
-        <div className='interviewBank-transcriptCard'>
+        <div className='interviewBank-transcriptCard' style={{marginBottom: '30px'}}>
             <a href="#" style={{textDecoration: 'none', color: 'black'}} onClick={handleClick}>
                 <div className='interviewBank-transcriptCardDetails'>
                     <TranscriptLabels name={name} date={date} />
